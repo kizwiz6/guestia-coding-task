d050e0b7300d27685231a80814228e6fc852e290
Richard Saunders <richard.saunders@guestia.co.uk>
2021-04-15 15:10:31 +0100

    Add project files.

---
+++ GuestiaCodingTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using GuestiaCodingTask.Data;
+using GuestiaCodingTask.Helpers;
+using System;
+using System.Linq;
+
+namespace GuestiaCodingTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbInitialiser.CreateDb();
+
+
+        }
+    }
+}
