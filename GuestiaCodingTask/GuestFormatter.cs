6df2d7e85fe99b281ba27877995df66120bb71e8
kizwiz <kizwiz@hotmail.co.uk>
2024-10-21 20:50:08 +0100

    Refactor Program.cs to use GuestRepository and GuestFormatter (to adhere to SRP).
    Refactored Program.cs to delegate responsibilities to new classes,
    GuestRepository and GuestFormatter, for improved code organization
    and separation of concerns. Moved GetUnregisteredGuestsGrouped to
    GuestRepository and FormatGuestName to GuestFormatter. Updated
    Program.cs to create and use instances of these new classes.

---
+++ GuestiaCodingTask/GuestFormatter.cs
@@ -0,0 +1,33 @@
+﻿using GuestiaCodingTask.Data;
+using GuestiaCodingTask.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestiaCodingTask
+{
+    /// <summary>
+    /// Handles the formatting of guest names based on their group specification.
+    /// </summary>
+    public class GuestFormatter
+    {
+        /// <summary>
+        /// Formats the guest's name based on the display format specified in the GuestGroup.
+        /// </summary>
+        /// <param name="guest">The guest whose name needs to be formatted.</param>
+        /// <returns>A formatted string representing the guest's name.</returns>
+        public string FormatGuestName(Guest guest)
+        {
+            var formatType = guest.GuestGroup?.NameDisplayFormat;
+
+            return formatType switch
+            {
+                NameDisplayFormatType.UpperCaseLastNameSpaceFirstName => $"{guest.LastName.ToUpper()} {guest.FirstName}",
+                NameDisplayFormatType.LastNameCommaFirstNameInitial => $"{guest.LastName}, {guest.FirstName[0]}.",
+                _ => $"{guest.FirstName} {guest.LastName}" // Default case
+            };
+        }
+    }
+}
