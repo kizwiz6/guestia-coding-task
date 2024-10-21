5068d43599cd934d84942793b26286a24ac4cc81
kizwiz <kizwiz@hotmail.co.uk>
2024-10-21 20:03:11 +0100

    Add method to fetch and count unregistered guests
    Included System.Collections.Generic for List<T> support.
    Added GetUnregisteredGuests method to Program class.
    Main method now prints count of unregistered guests.

--- GuestiaCodingTask/Program.cs
+++ GuestiaCodingTask/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GuestiaCodingTask.Data;
 using GuestiaCodingTask.Helpers;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GuestiaCodingTask
@@ -10,8 +11,22 @@ namespace GuestiaCodingTask
         static void Main(string[] args)
         {
             DbInitialiser.CreateDb();
+            var unregisteredGuests = GetUnregisteredGuests();
+            Console.WriteLine($"Number of unregistered guests: {unregisteredGuests.Count}");
 
+        }
 
+        /// <summary>
+        /// Retrieves all guests that have not registered yet.
+        /// </summary>
+        static List<Guest> GetUnregisteredGuests()
+        {
+            using (var context = new GuestiaContext())
+            {
+                return context.Guests
+                    .Where(g => !g.RegistrationDate.HasValue)
+                    .ToList();
+            }
         }
     }
 }
